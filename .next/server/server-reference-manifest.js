1d7e8892876b069331af0993f79b04c281c8729e
Charljay <jaydul1744@gmail.com>
2026-04-29 19:19:33 +0800

    Add Supabase client setup

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"K0iUFYULKCV0WQ4asw1UQ7EUUtANzi1m8rynMGZOfMc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
